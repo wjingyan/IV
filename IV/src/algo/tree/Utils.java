9d11046ecdd797701c8b97156d4ede55e374620a
wjingyan <freecady@gmail.com>
2014-08-21 10:50:42 +0800

    BST preorder serializer/deserializer     _30_    /    \   10    20  /     /  \ 50    45  35 to/from 30 10 50 # # # 20 45 # # 35 # #

--- IV/src/algo/tree/Utils.java
+++ IV/src/algo/tree/Utils.java
@@ -1,8 +1,21 @@
 package algo.tree;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 import lc.basic.TreeNode;
 
 public class Utils {
+
+	public static void main(String[] args) {
+		TreeNode t1 = preOrderDeserializer("30 10 50 # # # 20 45 # # 35 # #");
+		//printInorder(t1);
+		StringBuilder sb = new StringBuilder();
+		preOrderSerializer(t1, sb);
+		System.out.println(sb.toString());
+	}
+
 	public static void printInorder(TreeNode root) {
 		if (root == null) {
 			return;
@@ -10,5 +23,36 @@ public class Utils {
 		printInorder(root.left);
 		System.out.print(root.val + " ");
 		printInorder(root.right);
+	}
+
+	public static void preOrderSerializer(TreeNode node, StringBuilder str) {
+		if (node == null) {
+			str.append("# ");
+			return;
+		}
+		str.append(node.val + " ");
+		preOrderSerializer(node.left, str);
+		preOrderSerializer(node.right, str);
+	}
+
+	public static TreeNode preOrderDeserializer(String str) {
+		if (str == null || str.length() == 0) {
+			return null;
+		}
+		Queue<String> list = new LinkedList<String>(Arrays.asList(str.split(" ")));
+		return preOrderDeserializerHelper(list);
+	}
+
+	public static TreeNode preOrderDeserializerHelper(Queue<String> list) {
+		String valStr = list.poll();
+		if (valStr.equals("#")) {
+			return null;
+		} else {
+			int val = Integer.parseInt(valStr);
+			TreeNode node = new TreeNode(val);
+			node.left = preOrderDeserializerHelper(list);
+			node.right = preOrderDeserializerHelper(list);
+			return node;
+		}
 	}
 }
